f0c57a68c1468104a73fc87ecdc94ee87fbdf293
Sabinaxon08 <sabdugaffarovasabina@gmaSabinaxon08il.com>
2024-07-26 14:53:00 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+let body = document.querySelector('body')
+body.classList.add("bg-blue-900", "text-white", "flex", "h-screen", "justify-center", 'items-center', "text-4xl")
+
+let ism = prompt("ism yozing")
+let text = document.createElement('p')
+text.innerHTML = ism
+body.append(text)
+
+
+
+
+
+
+
